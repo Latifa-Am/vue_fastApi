aa3dfadf2b79da5f0f80126dce9f56303fe3feca
Latifa_am <105515930+Latifa-Am@users.noreply.github.com>
2022-12-04 21:09:00 +0100

    create the database models and generate migrations for them
    -->Tortoise is used for the ORM and Aerich for managing database migrations.
    
    -->We add a new service for Postgres to the docker-compose.yml file.
    
    -->We create our models 'Users' and 'Notes' with one-to-many relationship. Then, we specify the configuration for Tortoise and Aerich in the database/config.py file.
    
    -->To initialize the connection between the models and the database, we run the command: docker-compose exec backend aerich init -t src.database.config.TORTOISE_ORM
    
    -->To generate a migration file, we run the command: docker-compose exec backend aerich init-db
    
    --> We update our database after making changes on our models, we use the following commands:
    ***docker-compose exec backend aerich migrate
    ***docker-compose exec backend aerich upgrade

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -2,15 +2,21 @@ from fastapi import FastAPI
 #add corsmiddleware
 from fastapi.middleware.cors import CORSMiddleware
 
+from src.database.register import register_tortoise
+from src.database.config import TORTOISE_ORM
+
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:8080"],
+    allow_origins = ["*"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"],
 )
+
+register_tortoise(app, config=TORTOISE_ORM, generate_schemas=False)
+
 @app.get("/")
 def home():
     return "Hello, World!"
