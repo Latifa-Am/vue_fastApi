76e5e46aae24f4928344e6aa3775afa0e89139ba
Latifa_am <105515930+Latifa-Am@users.noreply.github.com>
2022-12-04 16:22:07 +0100

    add CORSMiddleware
    CORSMiddleware is required to make cross-origin requests (requests from a different protocol, IP address, domain name, or port).
    
    We also need to enable Cross Origin Resource Sharing (CORS). This is necessary since the frontend will run at http://localhost:8080.

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
-
+#add corsmiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["http://localhost:8080"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
 @app.get("/")
 def home():
     return "Hello, World!"
