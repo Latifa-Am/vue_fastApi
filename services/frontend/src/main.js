3e0d946410073fed78159cc0533da035539dd20a
Latifa_am <105515930+Latifa-Am@users.noreply.github.com>
2022-12-05 04:50:07 +0100

    add vuex to manage the state globally

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue';
 
 import App from './App.vue';
 import router from './router';
+import store from './store';
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "http://localhost:5000/"; //the fastApi backend
@@ -12,5 +13,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app');
