06e873e62af54eeacffb912da93bef52ff3c01ca
Latifa_am <105515930+Latifa-Am@users.noreply.github.com>
2022-12-05 09:56:25 +0100

    manage unauthorized users and redirect to the login instead
    If the user is not logged in and want to access to the profile page, the user should be redirected to the login page because of the requiresAuth specified in the meta for this route.

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:8080"],
+    allow_origins = ["http://localhost:8080", "http://localhost:5000"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"],
--- services/frontend/src/router/index.js
+++ services/frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
+import store from '@/store';
 import Dashboard from '@/views/Dashboard';
 import EditNote from '@/views/EditNote';
 import Home from '@/views/Home.vue';
@@ -61,4 +62,15 @@ const router = new VueRouter({
   routes,
 });
 
+router.beforeEach((to, from, next) =>{
+  if(to.matched.some(record => record.meta.requiresAuth)){
+    if(store.getters.isAuthenticated){
+      next();
+      return;
+    }
+    next('/login');
+  }else{
+    next();
+  }
+});
 export default router;
